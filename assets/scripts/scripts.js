2eb8f1d24a55fe0f3a68890ecbc080d0b4ab2986
andeodim <adimatulac.eh@gmail.com>
2019-05-23 01:02:35 -0700

    added the rest of the pages

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,53 +1,8 @@
+// add scripts for:
+//  - dynamic content addition (into cards/tiles?)
+//  - navbar hide on scroll
 
-
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar-menu").style.top = "0";
-//   } else {
-//     document.getElementById("navbar-menu").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
+// to load navbar for every page
+$(function() {
+    $("#nav-placeholder").load("navigation.html");
 });
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        // $('header#nav-down').removeAttr('id').attr('id', 'nav-up');
-        $('nav').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            // $('header#nav-up').removeAttr('id').attr('id', 'nav-down');
-            $('nav').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
-}
