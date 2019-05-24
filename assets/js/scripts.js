c0252f2f2333560ca0199d3275b895b447103ae0
andeodim <adimatulac.eh@gmail.com>
2019-05-24 13:21:13 -0700

    adding jquery for page modularity

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,19 @@
 //  - navbar hide on scroll
 
 // to load navbar for every page
-$(function(){
-    $('#nav-placeholder').load('navigation.html');
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let 
+// });
+
+// jQuery.noConflict();
+//      // Use jQuery via jQuery() instead of via $()
+//      jQuery(document).ready(function(){
+//        jQuery("attribution-container").hide();
+//      });  
+
+$(document).ready(function(){
+    $("#nav-placeholder").load("navigation.html", function() {
+        alert("this was load was performed");
+    });
 });
