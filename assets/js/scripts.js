19df38e1f3ba12a47f3614dc435bebf2c8845b94
andeodim <adimatulac.eh@gmail.com>
2019-05-25 00:10:39 -0700

    added hiding navbar on scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,3 +8,49 @@
 
 //     $("#foot-placeholder").load("footer.html");
 // });
+
+// $(window).scroll(function(e) {
+
+//     // add/remove class to navbar when scrolling to hide/show
+//     var scroll = $(window).scrollTop();
+//     if (scroll >= 100) {
+//         $(".navbar").addClass("navbar-hide");
+//     } else {
+//         $(".navbar").removeClass("navbar-hide");
+//     }
+
+// });
+
+let didScroll;
+let lastScrollTop = 0;
+let delta = 5;
+let navbarHeight = $("nav").outerHeight();
+
+$(window).scroll(function(event) {
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+     hasScrolled();
+      didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+  let st = $(this).scrollTop();
+      
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+      
+    if (st > lastScrollTop && st > navbarHeight){
+    // scroll down
+      $("nav").addClass("navbar-hide");
+    } else {
+    // scroll up
+    if(st + $(window).height() < $(document).height()) {
+      $("nav").removeClass("navbar-hide");
+    }
+  }   
+  lastScrollTop = st;
+} 
