bbcb467ec0457ae3225dc1dcab2c223e5eec979c
Angelli Dimatulac <angellidimatulac@gmail.com>
2019-08-16 05:27:20 -0700

    added lots of stuff

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,56 +1,52 @@
 // add scripts for:
 //  - dynamic content addition (into cards/tiles?)
-//  - navbar hide on scroll
 
-// to load navbar for every page - (jk too many problems)
-// $(document).ready(function(){
-//     $("#nav-placeholder").load("navigation.html");
+// hiding navbar on scroll
 
-//     $("#foot-placeholder").load("footer.html");
+// let didScroll;
+// let lastScrollTop = 0;
+// let delta = 5;
+// let navbarHeight = $("nav").outerHeight();
+
+// $(window).scroll(function(event) {
+//   didScroll = true;
 // });
 
-// $(window).scroll(function(e) {
-
-//     // add/remove class to navbar when scrolling to hide/show
-//     var scroll = $(window).scrollTop();
-//     if (scroll >= 100) {
-//         $(".navbar").addClass("navbar-hide");
-//     } else {
-//         $(".navbar").removeClass("navbar-hide");
+// setInterval(function() {
+//   if (didScroll) {
+//      hasScrolled();
+//       didScroll = false;
 //     }
+// }, 250);
 
-// });
+// function hasScrolled() {
+//   let st = $(this).scrollTop();
+      
+//     if (Math.abs(lastScrollTop - st) <= delta)
+//       return;
+      
+//     if (st > lastScrollTop && st > navbarHeight){
+//     // scroll down
+//       $("nav").addClass("navbar-hide");
+//     } else {
+//     // scroll up
+//     if(st + $(window).height() < $(document).height()) {
+//       $("nav").removeClass("navbar-hide");
+//     }
+//   }   
+//   lastScrollTop = st;
+// } 
 
-let didScroll;
-let lastScrollTop = 0;
-let delta = 5;
-let navbarHeight = $("nav").outerHeight();
+var $root = $('html, body');
 
-$(window).scroll(function(event) {
-  didScroll = true;
+$('a[href^=\\#]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+
+    event.preventDefault();
 });
-
-setInterval(function() {
-  if (didScroll) {
-     hasScrolled();
-      didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-  let st = $(this).scrollTop();
-      
-    if (Math.abs(lastScrollTop - st) <= delta)
-      return;
-      
-    if (st > lastScrollTop && st > navbarHeight){
-    // scroll down
-      $("nav").addClass("navbar-hide");
-    } else {
-    // scroll up
-    if(st + $(window).height() < $(document).height()) {
-      $("nav").removeClass("navbar-hide");
-    }
-  }   
-  lastScrollTop = st;
-} 
