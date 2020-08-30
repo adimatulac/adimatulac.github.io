cecfaccc26a3274706656f41cd353b6d8f63b72e
angelli <angellidimatulac@gmail.com>
2020-08-30 14:04:06 -0700

    add comments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,14 @@
 // add scripts for:
 //  - dynamic content addition (into cards/tiles?)
 
+// add class to navbar hamburger
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
 	});
 });
 
+// ease-in behaviour for photos
 [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
 	img.setAttribute('src', img.getAttribute('data-src'));
 	img.onload = function() {
